f3021d853523f4b8fe4d499a7360eb645bf7a72e
Carl Bergado <115414567+Carts1024@users.noreply.github.com>
2025-07-20 19:17:39 +0800

    Add game assets and remove symlink in web app
    Added various image, audio, and configuration files for landing page, game menu, leaderboards, team developers, settings, teams, select team, inventory, victory, defeat, draw, behaviors, loading, logo, match, sprites, and website page. Removed the symlink to shared assets and updated favicon.ico to support direct asset management within the web app.

---
+++ apps/web/public/assets/Sprites/Hero_P1-anim.ts
@@ -0,0 +1,3 @@
+// The constants with the animation keys.
+
+export const ANIM_HERO_P1_WALKHERO = "hero_P1-walkHero";
---
+++ apps/web/public/assets/Sprites/Hero_P1-anim_IDLE.ts
@@ -0,0 +1,3 @@
+// The constants with the animation keys.
+
+export const ANIM_IDLEHERO = "IDLEHero";
