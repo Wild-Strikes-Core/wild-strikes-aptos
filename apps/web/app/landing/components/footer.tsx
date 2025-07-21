f3ad5c2a482bb90f8f3249df633db9a7912db2ae
John Paulo Battung <126607972+sSquishy@users.noreply.github.com>
2025-07-21 16:38:43 +0800

    Adjusted Landing Page
    Defects:
    
    Home defects
    - match and remove anti aliasing
    
    Adjusted Play Button Animation and Hover
    
    Adjusted Footer Position

--- apps/web/app/landing/components/footer.tsx
+++ apps/web/app/landing/components/footer.tsx
@@ -12,78 +12,80 @@ export default function Footer() {
       </Head>
 
       <footer className="footer">
-        <div className="footer-content">
-          {/* Quick Links Section */}
-          {/* <div className="footer-section">
-            <h3>QUICK LINKS</h3>
-            <div className="footer-links">
-              <div className="footer-column">
-                <a href="#home">HOME</a>
-                <a href="#about">ABOUT</a>
+        <div className="footer-wrapper">
+          <div className="footer-content">
+            {/* Quick Links Section */}
+            {/* <div className="footer-section">
+              <h3>QUICK LINKS</h3>
+              <div className="footer-links">
+                <div className="footer-column">
+                  <a href="#home">HOME</a>
+                  <a href="#about">ABOUT</a>
+                </div>
+                <div className="footer-column">
+                  <a href="#social">SOCIAL</a>
+                  <a href="#mint">MINT</a>
+                </div>
               </div>
-              <div className="footer-column">
-                <a href="#social">SOCIAL</a>
-                <a href="#mint">MINT</a>
+            </div> */}
+            <div className="">
+              <Image
+                src="/assets/Website Page/logo.png"
+                alt="Wild Strikes Logo"
+                width={150}
+                height={50}
+                className="overview-logo"
+              />
+            </div>
+            
+            {/* Papers Section */}
+            <div className="footer-section">
+              <h3>PAPERS</h3>
+              <div className="footer-links">
+                <div className="footer-column">
+                  <a href="https://wild-strikes-1.gitbook.io/wild-strikes/?fbclid=IwY2xjawLnCq1leHRuA2FlbQIxMABicmlkETFYdHoxSkJac3liRHFKMjcxAR7dbuzzkslH6c6yVFXvKIjlriQALsWoPUOqZYQYyLEQfIked26fs5bE1UncSw_aem_GCN9mVULQZFDe010jAd8SQ" target="_blank" rel="noopener noreferrer">
+                    WHITEPAPER
+                  </a>
+                  <a href="https://codeofconduct.com" target="_blank" rel="noopener noreferrer">
+                    CODE OF CONDUCT
+                  </a>
+                </div>
+                <div className="footer-column">
+                  <a href="https://termsofservice.com" target="_blank" rel="noopener noreferrer">
+                    TERMS OF SERVICE
+                  </a>
+                  <a href="https://privacypolicy.com" target="_blank" rel="noopener noreferrer">
+                    PRIVACY POLICY
+                  </a>
+                </div>
               </div>
             </div>
-          </div> */}
-          <div className="">
-            <Image
-              src="/assets/Website Page/logo.png"
-              alt="Wild Strikes Logo"
-              width={280}
-              height={50}
-              className="overview-logo"
-            />
-          </div>
-          
-          {/* Papers Section */}
-          <div className="footer-section">
-            <h3>PAPERS</h3>
-            <div className="footer-links">
-              <div className="footer-column">
-                <a href="https://wild-strikes-1.gitbook.io/wild-strikes/?fbclid=IwY2xjawLnCq1leHRuA2FlbQIxMABicmlkETFYdHoxSkJac3liRHFKMjcxAR7dbuzzkslH6c6yVFXvKIjlriQALsWoPUOqZYQYyLEQfIked26fs5bE1UncSw_aem_GCN9mVULQZFDe010jAd8SQ" target="_blank" rel="noopener noreferrer">
-                  WHITEPAPER
-                </a>
-                <a href="https://codeofconduct.com" target="_blank" rel="noopener noreferrer">
-                  CODE OF CONDUCT
-                </a>
-              </div>
-              <div className="footer-column">
-                <a href="https://termsofservice.com" target="_blank" rel="noopener noreferrer">
-                  TERMS OF SERVICE
-                </a>
-                <a href="https://privacypolicy.com" target="_blank" rel="noopener noreferrer">
-                  PRIVACY POLICY
-                </a>
-              </div>
-            </div>
-          </div>
 
-          {/* Socials Section */}
-          <div className="footer-section socials-section">
-            <h3>SOCIALS</h3>
-            <div className="social-icons">
-              <a href="https://www.facebook.com/wildstrikess" target="_blank" rel="noopener noreferrer" className="social-icon">
-                <i className="ri-facebook-circle-fill"></i>
-                <Image
-                  src="/assets/Website Page/facebook.png"
-                  alt="facebook"
-                  width={30}
-                  height={1}
-                />
-              </a>
-              <a href="https://x.com/Wild_Strikes" target="_blank" rel="noopener noreferrer" className="social-icon">
-                <i className="ri-twitter-x-line"></i>
-                <Image
-                  src="/assets/Website Page/twitter.png"
-                  alt="twitter"
-                  width={30}
-                  height={1}
-                />
-              </a>
+            {/* Socials Section */}
+            <div className="footer-section socials-section">
+              <h3>SOCIALS</h3>
+              <div className="social-icons">
+                <a href="https://www.facebook.com/wildstrikess" target="_blank" rel="noopener noreferrer" className="social-icon">
+                  <i className="ri-facebook-circle-fill"></i>
+                  <Image
+                    src="/assets/Website Page/facebook.png"
+                    alt="facebook"
+                    width={30}
+                    height={1}
+                  />
+                </a>
+                <a href="https://x.com/Wild_Strikes" target="_blank" rel="noopener noreferrer" className="social-icon">
+                  <i className="ri-twitter-x-line"></i>
+                  <Image
+                    src="/assets/Website Page/twitter.png"
+                    alt="twitter"
+                    width={30}
+                    height={1}
+                  />
+                </a>
+              </div>
+              <p className="copyright">@2025 WildStrikes</p>
             </div>
-            <p className="copyright">@2025 WildStrikes</p>
           </div>
         </div>
       </footer>
