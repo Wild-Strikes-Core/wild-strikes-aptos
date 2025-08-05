0362796835acba776cf139ff0f8b8dd0e2808a35
Carl Bergado <115414567+Carts1024@users.noreply.github.com>
2025-08-06 00:25:47 +0800

    Refactor backend and add whitelist feature to web
    Removed default NestJS controller and service from backend, added @nestjs/config for global configuration, and updated main.ts to use environment port. Introduced new whitelist feature in web app with supporting files for email whitelist functionality.

--- apps/backend/wildstrikes-backend/src/app.module.ts
+++ apps/backend/wildstrikes-backend/src/app.module.ts
@@ -1,10 +1,14 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+      ConfigModule.forRoot({
+      isGlobal: true, // makes ConfigModule available everywhere without re-import
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
